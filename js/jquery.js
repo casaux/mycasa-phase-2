286a5e54e5b4256d4bc048c269daf69bdf584061
James Offer <james.offer@casa.gov.au>
2019-03-04 14:17:43 +1100

    passport

--- js/jquery.js
+++ js/jquery.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 
 	menu = '<a href="#"><div class="casa-menu-menu"><img src="img/menu.svg" width="24"><span class="casa-menu-label">Menu</span></div><div class="casa-menu-close"><img src="img/close.svg" width="24"><span class="casa-menu-label">Close</span></div></a>';
 	
-	menu += '<div class="casa-menu-content">...</div>';
+	menu += '<div class="casa-menu-content"><div class="casa-menu-item"><a href="#">Profile</a></div><div class="casa-menu-item"><a href="#">Switch profiles</a></div><div class="casa-menu-item"><a href="#">Sign out</a></div></div>';
 	
 	$(".casa-menu").append(menu);
 	
@@ -16,9 +16,22 @@ $(document).ready(function(){
 	  if($(".casa-menu-menu").css("display")=="block"){
 	  	$(".casa-menu-menu").css("display","none");
 	  	$(".casa-menu-close").css("display","block");
+		  
+		  
+		  
+		var menuHeight = $(document).height() - parseInt($(".casa-menu").outerHeight());
+		$(".casa-menu-content").css("height",menuHeight+"px");
+		$(".casa-menu-content").css("top",$(".casa-menu").outerHeight());
+		$(".casa-menu-content").css("display","block");
+		  
+		  
+		  
+		  
 	  } else {
 		$(".casa-menu-menu").css("display","block");
 	  	$(".casa-menu-close").css("display","none");
+		$(".casa-menu-content").css("display","none");
+
 	  }
 	  
 	  
