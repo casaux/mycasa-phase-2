2957015305fd9528c2ef229271cd200d386d4a89
CASA UX <jamesoffer@MacBook-Pro-2.lan>
2019-03-07 21:30:06 +1100

    update

--- js/jquery.js
+++ js/jquery.js
@@ -1,47 +1,79 @@
 $(document).ready(function(){
-
-
-
-
-	menu = '<a href="#"><div class="casa-menu-menu"><img src="img/menu.svg" width="24"><span class="casa-menu-label">Menu</span></div><div class="casa-menu-close"><img src="img/close.svg" width="24"><span class="casa-menu-label">Close</span></div></a>';
-	
-	menu += '<div class="casa-menu-content"><div class="casa-menu-item"><a href="#">Profile</a></div><div class="casa-menu-item"><a href="#">Switch profiles</a></div><div class="casa-menu-item"><a href="#">Sign out</a></div></div>';
-	
-	$(".casa-menu").append(menu);
 	
 	
-	$(".casa-menu").bind( "click", function() {
-	  
-	  
-	  if($(".casa-menu-menu").css("display")=="block"){
-	  	$(".casa-menu-menu").css("display","none");
-	  	$(".casa-menu-close").css("display","block");
-		  
-		  
-		  
-		var menuHeight = $(document).height() - parseInt($(".casa-menu").outerHeight());
-		$(".casa-menu-content").css("height",menuHeight+"px");
-		$(".casa-menu-content").css("top",$(".casa-menu").outerHeight());
-		$(".casa-menu-content").css("display","block");
-		  
-		  
-		  
-		  
-	  } else {
-		$(".casa-menu-menu").css("display","block");
-	  	$(".casa-menu-close").css("display","none");
-		$(".casa-menu-content").css("display","none");
-
-	  }
-	  
-	  
-	  
+	$("#dd-close").css("display","none");
+	
+	
+	$(".menu").bind("click",function(event){
+	
+	
+		
+		if($("#dd-close").css("display")=="block"){
+			$("#dd-close").css("display","none");
+			$("#dd-menu").css("display","block");
+		} else {
+			$("#dd-close").css("display","block");
+			$("#dd-menu").css("display","none");		
+		}
+		
+			return false;
+		event.preventDefault();
+	
 	});
 	
 	
+	$(".casa-dashboard-item").html()
 	
 	
+	$(".casa-dashboard-item").bind("click",function(){
+		
+		var link = $(this).find("a");
+		
+		link = (link).attr("href");
+		
+		if(link){
+			location.href=link;
+		}
+		
+	});
 	
+	$("a.au-btn").bind("click",function(){
+		
+		
+		
+		//$( "body" ).prepend( "<div id='modal'></div>" );
+
+		
+		/*
+		
+		var href = $(this).attr('href');
+
+
+		$(this).html('Saving...');
+		
+		$("*").css("cursor", "progress");
+
+		
+             // Delay setting the location for one second
+        setTimeout(function() {window.location = href}, 2000);
+        return false;
+		*/
+		
+	});
+	
+	
+	$(".cancel").bind("click",function(){
+		
+		
+		if (confirm('Are you sure you want to cancel your ARN application?')) {
+			
+			location.href="cancelled.html";
+			
+		} else {
+			// Do nothing!
+		}
+		
+	});
 	
 	
 });
